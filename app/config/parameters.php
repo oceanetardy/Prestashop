7303ed2f417892e868ba4fb25aeb1eb09aecaf67
oceane tardy <tardy.oceane42@gmail.com>
2023-03-24 22:03:20 +0100

    [+] Init project

---
+++ app/config/parameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'ZIv219QFpTJWIRvxVeTRccast0QEe7FiyeLnHwLmPBBc9H7OrXAAvnFSuPVZuF1S',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2023-03-24',
+    'locale' => 'fr-FR',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'KxomH8EAJR5uoj2Uy2Om2ZRFbuV2nbpGJ2ZmGA9AinJA1C0LwytQBAKBuxksInxW',
+    'cookie_iv' => '1H96N4qqZ4WyFTr24RFijTT28d7chx6z',
+    'new_cookie_key' => 'def000008c5d3d7b3acb8867bab8be88a14a9572223a1ba249e985f6b2853450fcc9f83443cee1549325ac0738eb9e4c562605aee4201ae02d03265c49801290a183887b',
+  ),
+);
